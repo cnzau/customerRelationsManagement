f954e146efafa3947b8f8d2e0986ff50bd572a03
cnzau <clement.nzau@gmail.com>
2015-12-09 20:47:02 +0300

    mongoose model to handle users

---
+++ app/models/user.js
@@ -0,0 +1,38 @@
+//grab packages needed for user model
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	bcrypt = require('bcrypt-nodejs');
+
+//user schema
+var UserSchema = new Schema({
+	name: String,
+	username: { type: String, required: true, index: { unique: true } },
+	password: { type: String, required: true, select: false }
+});
+
+//hash passwords before the user is saved
+UserSchema.pre('save', function(next) {
+	var user = this;
+
+	//hash password only if it has been changed or user is new
+	if (!user.isModified('password')) reurn next();
+
+	//generate the hash
+	bcrypt.hash(user.password, null, null function(err, hash) {
+		if (err) return next(err);
+
+		//change the passord to the hashed version
+		user.password = hash;
+		next();
+	});	
+})
+
+//method to compare a given password with the database hash
+UserSchema.method.comparePassword = function(password) {
+	var user = this;
+
+	return bcrypt.compareSync(password, user.password);
+};
+
+//return the model
+module.exports = mongoose.model('User', UserSchema);

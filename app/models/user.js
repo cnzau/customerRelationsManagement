ff7844baf57d77e41a6e5e1ecb071531820d6dcb
cnzau <clement.nzau@gmail.com>
2015-12-14 15:08:08 +0300

    debugged compare function method to methods

--- app/models/user.js
+++ app/models/user.js
@@ -28,7 +28,7 @@ UserSchema.pre('save', function(next) {
 })
 
 //method to compare a given password with the database hash
-UserSchema.method.comparePassword = function(password) {
+UserSchema.methods.comparePassword = function(password) {
 	var user = this;
 
 	return bcrypt.compareSync(password, user.password);

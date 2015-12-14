ffa7d3f6a47a7e4279a68dfe2eaa0e695d045fa3
cnzau <clement.nzau@gmail.com>
2015-12-14 15:12:33 +0300

    authenticate a user and give a token

--- server.js
+++ server.js
@@ -42,6 +42,53 @@ app.get('/', function(req, res) {
 //get an instance of the express router
 var apiRouter = express.Router();
 
+//authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRouter.post('/authenticate', function(req, res) {
+	
+	//find the user
+	//select the name, username and password explicitly
+	User.findOne({
+		username: req.body.username
+	}).select('name username password').exec(function(err, user) {
+		if (err) throw err;
+
+		//no user with that username was found
+		if (!user) {
+			res.json({
+				success: false,
+				message: 'Authentication failed. User not found.'
+			});
+		} else if (user) {
+
+			//check if password matches
+			var validPassword = user.comparePassword(req.body.password);
+			if (!validPassword) {
+				res.json({
+					success: false,
+					message: 'Authentication failed. Wrong password.'
+				});
+			} else {
+
+				//if user is found and password is right
+				//create token
+				var token = jwt.sign({
+					name: user.name,
+					username: user.username
+				}, superSecret, {
+					expiresInMinutes: 1440	//expires in 24 hours
+				});
+
+				//return the information including token as JSON
+				res.json({
+					success: true,
+					message: 'Enjoy your token!',
+					token: token
+				});
+			}
+		}
+	});
+});
+
 //middleware to use for all requests
 apiRouter.use(function(req, res, next) {
 	//do logging

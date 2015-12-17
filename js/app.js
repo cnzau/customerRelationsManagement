6a739d79361c0e8d03952f899ae9fc8bdabce931
cnzau <clement.nzau@gmail.com>
2015-12-17 20:44:58 +0300

    Creating and processing forms. Angular function to process form adding to list->app.js. HTML form to wire wire with angular function->index.html.

--- js/app.js
+++ js/app.js
@@ -18,4 +18,20 @@ angular.module('firstApp', [])
 		{ name: 'Yoga 2 Pro', color: 'Gray', nerdness: 6 },
 		{ name: 'Chromebook', color: 'Black', nerdness: 5 }
 	];
+
+	//information that comes from our form
+	vm.computerData = {};
+
+	vm.addComputer = function() {
+
+		//add a computer to the list
+		vm.computers.push({
+			name: vm.computerData.name,
+			color: vm.computerData.color,
+			nerdness: vm.computerData.nerdness
+		});
+
+		//after our computer has been added, clear the form
+		vm.computerData = {};
+	};
 });

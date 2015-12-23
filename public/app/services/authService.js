e399da24a774a80603d661838af90b8d8e3d8d0a
cnzau <clement.nzau@gmail.com>
2015-12-23 19:32:25 +0300

    added an interceptor factory: attaches the token to all HTTP request coming from frontend app.

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -17,10 +17,10 @@
       		username: username,
        		password: password
      	})       
-       	.success(function(data) {
-         	AuthToken.setToken(data.token);
-         	return data;
-       });
+       		.success(function(data) {
+         		AuthToken.setToken(data.token);
+         		return data;
+       		});
    	};
  
    	// log a user out by clearing the token
@@ -77,3 +77,40 @@
    	return authTokenFactory;
  
 })
+
+// ===================================================
+// application configuration to integrate token into requests
+// ===================================================
+.factory('AuthInterceptor', function($q, $location, AuthToken) {
+ 
+	var interceptorFactory = {};
+
+   	// this will happen on all HTTP requests
+	interceptorFactory.request = function(config) {
+ 
+    	// grab the token
+     	var token = AuthToken.getToken();
+ 
+     	// if the token exists, add it to the header as x-access-token
+     	if (token) 
+       	config.headers['x-access-token'] = token;
+     
+     	return config;
+   	};
+ 
+   	// happens on response errors
+   	interceptorFactory.responseError = function(response) {
+ 
+     	// if our server returns a 403 forbidden response
+     	if (response.status == 403) {
+       		AuthToken.setToken();
+       		$location.path('/login');
+     	}
+ 
+     	// return the errors from the server as a promise
+    	return $q.reject(response);
+   	};
+ 
+ 	return interceptorFactory;
+   
+});

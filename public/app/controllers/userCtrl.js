dea93336b18ded62a35b67e5a584f89743bde588
cnzau <clement.nzau@gmail.com>
2015-12-26 17:43:48 +0300

    Delete a user:function in our controller; button in our view

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -20,4 +20,23 @@ angular.module('userCtrl', ['userService'])
 				// bind the users that come back to vm.users
 				vm.users = data;
 			});
+
+		//function to delete a user
+		vm.deleteUser = function(id) {
+			vm.processing = true;
+
+			//accepts the user id as a parameter
+			User.delete(id)
+				.success(function(data) {
+
+					//get all users to update the table
+					//you can also set up your api
+					//to return the list of users with the delete call
+					User.all()
+						.success(function(data) {
+							vm.processing = false;
+							vm.users = data;
+						});
+				});
+		};
 	})

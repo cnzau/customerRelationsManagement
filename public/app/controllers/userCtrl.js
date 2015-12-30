9186649c15218909442b7e564a2ffc6f99eab31e
cnzau <clement.nzau@gmail.com>
2015-12-30 17:02:55 +0300

    created edit User controller. parameter passed in the URL; get info on page load; bind info to our form

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -61,4 +61,40 @@ angular.module('userCtrl', ['userService'])
 					vm.message = data.message;
 				});
 		};
-	})
+	});
+
+	// controller applied to user edit page
+	.controller('userEditController', function($routeParams, User) {
+ 
+		var vm = this;
+ 
+		// variable to hide/show elements of the view
+		// differentiates between create or edit pages
+		vm.type = 'edit';
+
+		// get the user data for the user you want to edit
+		// $routeParams is the way we grab data from the URL
+		User.get($routeParams.user_id)
+			.success(function(data) {
+				vm.userData = data;
+			});
+ 
+		// function to save the user
+		vm.saveUser = function() {
+			vm.processing = true;
+			vm.message = '';
+ 
+			// call the userService function to update 
+			User.update($routeParams.user_id, vm.userData)
+				.success(function(data) {
+					vm.processing = false;
+
+					// clear the form
+					vm.userData = {};
+ 
+					// bind the message from our API to vm.message
+					vm.message = data.message;
+				});
+		};
+ 
+	});
